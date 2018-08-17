c18b6cbc867b4b2b8f868ef0b8583bbd1f6bf727
vishavdeepsingh <vishavsandhu7700@gmail.com>
2018-08-17 19:10:41 +0530

    thanks

---
+++ ASSIGNMENT2.py.py
@@ -0,0 +1,32 @@
+1.
+print('VISHAV SANDHU')
+2.
+x='Home'
+y='work'
+print(x+y)
+3.
+print('Value for first variable')
+c=input()
+print('Value for second variable')
+d=input()
+print('Value for third variable')
+e=input()
+print('the value of first variable is '+c)
+print('the value of first variable is '+d)
+print('the value of first variable is '+e)
+4.
+print("Let's get started")
+5.
+s='Acadview'
+course='Python'
+fees=5000
+print('%s teaches %s course for Rs%d'%(s,course,fees))
+6.
+#Area Of Circle
+print('Enter the radius of the circle')
+radius=float(input())
+pi=3.14
+area=pi*radius*radius
+print('Area of the circle is = %.2f'%area)
+
+
